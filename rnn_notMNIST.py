0b808aa7c87c9b0d99c051ddfe6659187bdccc16
Chris Sheehan <csheehan@cbinsights.com>
2017-02-24 13:59:18 -0500

    notMNIST

--- rnn_notMNIST.py
+++ rnn_notMNIST.py
@@ -1,21 +1,22 @@
-import pickle
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops import rnn, rnn_cell
+
+import pickle
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix, roc_auc_score, precision_score, recall_score
 
 import logging
 import common
-logger = common.init_logging('rnn_sentiment.log')
+logger = common.init_logging('rnn_notMNIST.log')
 
-## Import X_vecs, y_vecs from fintech_XY.pickle
 logger.info('Loading pickled vecs...')
-# X_vecs, y_vecs = pickle.load(open('fintech_XY.pickle', 'rb'))
-# train_x, test_x, train_y, test_y = train_test_split(X_vecs, y_vecs)
-# train_x, test_x, train_y, test_y = train_test_split(X_vecs[:1000], y_vecs[:1000])
+data = pickle.load(open('notMNIST_data.pickle', 'rb'))
+Xdata = np.array([img[1] for img in data])
+ydata = np.array([img[2] for img in data])
 
-train_x, train_y, test_x, test_y = pickle.load(open('sentiment_set.pickle', 'rb'))
+logger.info('Train Test Split...')
+train_x, test_x, train_y, test_y = train_test_split(Xdata, ydata)
 train_x = np.array(train_x[:10000])
 train_y = np.array(train_y[:10000])
 test_x = np.array(test_x[:10000])
@@ -25,19 +26,17 @@ test_y = np.array(test_y[:10000])
 ytrain_sing = np.argmax(train_y, 1) == 0
 ytest_sing = np.argmax(test_y, 1) == 0
 
-hm_epochs = 5
-n_classes = 2
-batch_size = 129
+hm_epochs = 3
+n_classes = 10
+batch_size = 128
 
-# chunk_size = 25
-# n_chunks = 495
-chunk_size = 12
-n_chunks = 36
+chunk_size = 28
+n_chunks = 28
 rnn_size = 128
 
 logger.info('Init placeholder x,y.')
 x = tf.placeholder('float', [None, n_chunks, chunk_size])
-y = tf.placeholder('float', [None, n_classes])
+y = tf.placeholder('float', [None, 10])
 
 def recurrent_neural_network_model(x):
 	layer = dict(weights = tf.Variable(tf.random_normal([rnn_size, n_classes])), 
@@ -47,7 +46,7 @@ def recurrent_neural_network_model(x):
 	x = tf.reshape(x, [-1, chunk_size])
 	x = tf.split(0, n_chunks, x)
 
-	lstm_cell = rnn_cell.BasicLSTMCell(rnn_size, forget_bias = 0.5)
+	lstm_cell = rnn_cell.BasicLSTMCell(rnn_size)
 	outputs, states = rnn.rnn(lstm_cell, x, dtype = tf.float32)
 
 	output = tf.matmul(outputs[-1], layer['weights']) + layer['biases']
@@ -56,7 +55,7 @@ def recurrent_neural_network_model(x):
 
 def train_neural_network(x, y):
 	prediction = recurrent_neural_network_model(x)
-	cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits(prediction, y ))
+	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
 	optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 	with tf.Session() as sess:
@@ -73,17 +72,16 @@ def train_neural_network(x, y):
 
 				batch_x = np.array(train_x[start:end])
 				batch_y = np.array(train_y[start:end])
-				batch_x = batch_x.reshape((min(batch_size, len(batch_x)), n_chunks, chunk_size))
+				# batch_x = batch_x.reshape((min(batch_size, len(batch_x)), n_chunks, chunk_size))
 
 				_, c = sess.run([optimizer, cost], feed_dict = {x : batch_x, y : batch_y})
 				epoch_loss += c
 				ii += 1
-			print 'Epoch', epoch+1, 'completed out of', hm_epochs, '. loss:', epoch_loss
-			logger.info('Epoch %s completed out of %s. Loss: %s.' % (epoch+1, hm_epochs, epoch_loss))
+			print 'Epoch', epoch, 'completed out of', hm_epochs, 'loss:', epoch_loss
 
 		correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y,1))
 		accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-		accuracy_eval = accuracy.eval({x : test_x.reshape((-1, n_chunks, chunk_size)), y : test_y}, session = sess)
+		accuracy_eval = accuracy.eval({x : test_x.reshape((-1, n_chunks, chunk_size)), y : test_y})
 		print 'Accuracy:', accuracy_eval
 		logger.info('Accuracy: %s' % accuracy_eval)
 		yhat_test = np.argmax(sess.run(prediction, feed_dict = {x : test_x.reshape((-1, n_chunks, chunk_size)), y : test_y}), axis = 1)
